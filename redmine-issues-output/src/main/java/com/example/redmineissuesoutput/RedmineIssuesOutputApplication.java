17cf49b50d639a849c072ea520f7e43761a0a27f
matttttttso <u2.pinetail@gmail.com>
2022-02-24 13:44:06 +0900

    Initial Commit

---
+++ redmine-issues-output/src/main/java/com/example/redmineissuesoutput/RedmineIssuesOutputApplication.java
@@ -0,0 +1,13 @@
+package com.example.redmineissuesoutput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedmineIssuesOutputApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedmineIssuesOutputApplication.class, args);
+	}
+
+}
---
+++ redmine-issues-output/src/test/java/com/example/redmineissuesoutput/RedmineIssuesOutputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redmineissuesoutput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedmineIssuesOutputApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
