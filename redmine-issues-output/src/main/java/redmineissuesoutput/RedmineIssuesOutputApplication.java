03f469d91f16721589f8e46865aa2a9071759766
matttttttso <u2.pinetail@gmail.com>
2022-02-24 14:08:26 +0900

    パッケージ移動

--- redmine-issues-output/src/main/java/redmineissuesoutput/RedmineIssuesOutputApplication.java
+++ redmine-issues-output/src/main/java/redmineissuesoutput/RedmineIssuesOutputApplication.java
@@ -1,4 +1,4 @@
-package com.example.redmineissuesoutput;
+package redmineissuesoutput;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
