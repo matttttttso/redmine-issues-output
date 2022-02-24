665960fa0ed87cfe22dbc7a0c8cc76ca3a6bd568
matttttttso <u2.pinetail@gmail.com>
2022-02-24 16:28:57 +0900

    RedmineInfo application.yml以外から読込む場合

--- redmine-issues-output/src/main/java/redmineissuesoutput/domain/model/RedmineInfo.java
+++ redmine-issues-output/src/main/java/redmineissuesoutput/domain/model/RedmineInfo.java
@@ -1,19 +1,32 @@
 package redmineissuesoutput.domain.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-import lombok.Getter;
 import lombok.Setter;
 
-// application.ymlから読込む場合
+// application.yml以外から読込む場合
+@Configuration
+@PropertySource("classpath:/config/confidential.yml")
 @Component
 @ConfigurationProperties(prefix = "app.config.redmine")
 public class RedmineInfo {
-	@Getter
+	@Autowired
+	private Environment env;
+	
 	@Setter
 	private String url;
-	@Getter
 	@Setter
 	private String apiKey;
+	
+	public String getUrl() {
+		return env.getProperty("url");
+	}
+	public String getApiKey() {
+		return env.getProperty("apiKey");
+	}
 }
