a5702f335b6b9f65444065285dd2f0d189eddc23
matttttttso <u2.pinetail@gmail.com>
2022-02-25 17:12:30 +0900

    [WIP]値の格納先調べ

--- redmine-issues-output/src/main/java/redmineissuesoutput/app/controller/MainController.java
+++ redmine-issues-output/src/main/java/redmineissuesoutput/app/controller/MainController.java
@@ -1,5 +1,14 @@
 package redmineissuesoutput.app.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +18,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.taskadapter.redmineapi.RedmineException;
 import com.taskadapter.redmineapi.RedmineManager;
 import com.taskadapter.redmineapi.RedmineManagerFactory;
+import com.taskadapter.redmineapi.bean.CustomField;
+import com.taskadapter.redmineapi.bean.Issue;
+import com.taskadapter.redmineapi.bean.Project;
 import com.taskadapter.redmineapi.internal.Transport;
 
 import redmineissuesoutput.domain.model.RedmineInfo;
@@ -40,9 +53,39 @@ public class MainController {
 	 */
 	@RequestMapping(method = RequestMethod.GET)
 	String moveSearchView() {
-		String url = redmineInfo.getUrl();
-		String apiKey = redmineInfo.getApiKey();
 		RedmineManager redmineManager = RedmineManagerFactory.createWithApiKey(redmineInfo.getUrl(), redmineInfo.getApiKey());
+		List<Project> projectList = new ArrayList<>();
+		Project aki = null;
+		Project akiKadai = null;
+		List<Issue> akiTicketList = new ArrayList<>();
+		List<Issue> akiKadaiTicketList = new ArrayList<>();
+		Issue akiIssue0 = new Issue();
+		Collection<CustomField> customField = new HashSet<>();
+		List<Object> list = new ArrayList<>();
+		CustomField customFieldfuku = new CustomField();
+		try {
+			projectList = redmineManager.getProjectManager().getProjects();
+			projectList.get(0).getParentId();
+			aki = projectList.get(7);
+//			aki = projectList.get(13);
+			akiTicketList = redmineManager.getIssueManager().getIssues(aki.getIdentifier(), null);
+			akiIssue0 = akiTicketList.get(0);
+			customField = akiIssue0.getCustomFields();
+			customFieldfuku = akiIssue0.getCustomFieldByName("副担当者");
+			customFieldfuku = akiIssue0.getCustomFieldByName("部門");
+			customFieldfuku = akiIssue0.getCustomFieldByName("管理番号");
+			customFieldfuku = akiIssue0.getCustomFieldByName("発生日");
+			customFieldfuku = akiIssue0.getCustomFieldByName("検討案または結果");
+			customFieldfuku = akiIssue0.getCustomFieldByName("完了日");
+			customFieldfuku = akiIssue0.getCustomFieldByName("発信元担当者");
+			list = Arrays.asList(customField.toArray());;
+			akiIssue0.getDueDate();
+//			akiKadai = projectList.get(14);
+//			akiKadaiTicketList = redmineManager.getIssueManager().getIssues(akiKadai.getIdentifier(), akiKadai.getId());
+		} catch (RedmineException e) {
+			e.printStackTrace();
+//			Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, e);
+		}
 		Transport transport = redmineManager.getTransport();
 		return "redirect:/search";
 	}
