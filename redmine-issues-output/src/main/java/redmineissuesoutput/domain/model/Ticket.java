4893ee648636444a2918a68cd22df5937f3e75a7
matttttttso <u2.pinetail@gmail.com>
2022-03-02 17:02:03 +0900

    表示ずれ修正(担当者名、完了日)、副担当者表示漏れバグ修正 Ticket

--- redmine-issues-output/src/main/java/redmineissuesoutput/domain/model/Ticket.java
+++ redmine-issues-output/src/main/java/redmineissuesoutput/domain/model/Ticket.java
@@ -4,9 +4,11 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Objects;
 
 import com.taskadapter.redmineapi.bean.Issue;
+import com.taskadapter.redmineapi.bean.User;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -35,7 +37,7 @@ public class Ticket implements Serializable {
 	private String dueDate;			//	期限				期日				Issue.getDueDate()
 	private String completionDate;	//	回答または完了日	完了日			CustomField.getCustomFieldByName("完了日").getValue()
 	
-	public Ticket(Issue issue, int sec) {
+	public Ticket(Issue issue, int sec, List<User> userList) {
 		this.sequence = sec;
 		this.id = issue.getId();
 		this.tracker = issue.getTracker().getName();
@@ -47,12 +49,30 @@ public class Ticket implements Serializable {
 		this.originator = issue.getCustomFieldByName("発信元担当者").getValue();
 		this.status = issue.getStatusName();
 		this.agenda = issue.getDescription();
-		this.assigneeName = issue.getAssigneeName();
-		this.subAssigneeName = issue.getCustomFieldByName("副担当者").getValue();
+		if (Objects.nonNull(issue.getAssigneeName())) {
+			int endIdx = issue.getAssigneeName().indexOf(' ');
+			this.assigneeName = issue.getAssigneeName().substring(0, endIdx + 1);
+		}
+		if (issue.getCustomFieldByName("副担当者").getValues().size() > 0) {
+			StringBuilder sb = new StringBuilder();
+			int i = 0;
+			for (String id : issue.getCustomFieldByName("副担当者").getValues()) {
+				if (i != 0) {
+					sb.append("\n");
+					i++;
+				}
+				sb.append(userList.stream().filter(u -> Objects.equals(u.getId(), Integer.valueOf(id))).findFirst().get().getLastName());
+			}
+			this.subAssigneeName = sb.toString();
+		}
 		this.result = issue.getCustomFieldByName("検討案または結果").getValue();
 		if (Objects.nonNull(issue.getDueDate())) {
 			this.dueDate = new SimpleDateFormat("yyyy/MM/dd").format(issue.getDueDate());
 		}
-		this.completionDate = issue.getCustomFieldByName("完了日").getValue();
+		if (!issue.getCustomFieldByName("完了日").getValue().isEmpty()) {
+			this.completionDate = LocalDate
+					.parse(issue.getCustomFieldByName("完了日").getValue(), DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+					.format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+		}
 	}
 }
